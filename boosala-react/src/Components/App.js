cdc9ae9ec7b87ce36ed7c1ae77ec34675c791d96
ashley <msashleychen@gmail.com>
2018-05-13 01:21:47 -0400

    Added background, formatted homepage text

--- boosala-react/src/Components/App.js
+++ boosala-react/src/Components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
-import logo_front from  './images/logo_front.png';
-import logo_back from './images/logo_back.png';
+import './../css/App.css';
+import logo_front from  './../images/logo_front.png';
+import logo_back from './../images/logo_back.png';
 
 class App extends Component {
 
   constructor(props){
     super(props);
+    //Break up each text into english and arabic
     this.state = {
       language: 'english',
       heading:{
@@ -45,14 +46,16 @@ class App extends Component {
       <div className="App">
         {this.renderLanguageBar()}
         <div className="Homepage">
-          <div id="logo-div">
-              <img id="logo-back" class="logo" src={logo_front} alt="boosala logo"/>
-              <img id="logo-front" class="logo" src={logo_back} alt="boosala logo"/>
+          <div id="logo-div" className="column">
+              <img id="logo-back" className="logo" src={logo_back} alt="boosala logo"/>
+              <img id="logo-front" className="logo" src={logo_front} alt="boosala logo"/>
           </div>
-          <div id="heading">{this.state.heading[this.state.language]}</div>
-          <div id="title">{this.state.title[this.state.language]}</div>
-          <div id="caption">{this.state.caption[this.state.language]}</div>
-          <button id="startButton">{this.state.startButton[this.state.language]}</button>
+          <div className="text column">
+            <div id="heading">{this.state.heading[this.state.language]}</div>
+            <div id="title">{this.state.title[this.state.language]}</div>
+            <div id="caption">{this.state.caption[this.state.language]}</div>
+            <button id="startButton">{this.state.startButton[this.state.language]}</button>
+          </div>
         </div>
       </div>
     );
