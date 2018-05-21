61a3c23caace6450e1774a4a6ad00ce119a4f145
ashley <msashleychen@gmail.com>
2018-05-20 23:22:38 -0400

    Revert root rendering

--- boosala-react/src/index.js
+++ boosala-react/src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './Components/App';
-import Search from './Components/Search';
 
-
-ReactDOM.render(<Search />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
