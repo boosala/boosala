69049b1cfb1900bb6295ad19a9e5d5914915eb3e
Said <saidziani.pro@gmail.com>
2018-05-23 01:16:56 +0100

    Add states, get and set input values to Result page.

--- boosala-react/src/Components/Search.js
+++ boosala-react/src/Components/Search.js
@@ -6,6 +6,7 @@ import faceAr from './../images/faceAr.jpg';
 import arrowLeft from './../images/arrowLeft.png';
 //import $ from 'jquery'
 import App from './App';
+import Result from './Result';
 
 
 class Search extends Component {
@@ -13,6 +14,9 @@ class Search extends Component {
   constructor(props){
     super(props);
     this.backToHome = this.backToHome.bind(this);
+    this.loadResult = this.loadResult.bind(this);
+    this.updateUserNameValue = this.updateUserNameValue.bind(this);
+    this.updateTheirValue = this.updateTheirValue.bind(this);
     this.state = {
       language: this.props.language,
       text:{
@@ -27,6 +31,10 @@ class Search extends Component {
         english: faceEn,
         arabic: faceAr
       },
+      userName: null,
+      their: null,
+      location: null,
+      datetime: null,
       current: "search",
       backgroundColor: "white"
     }
@@ -36,30 +44,48 @@ class Search extends Component {
     this.setState({current: "home"});
   }
 
+  loadResult(){
+    this.setState({current: "result", 
+        their: this.state.their, 
+        userName: this.state.userName, 
+        location: this.state.location, 
+        datetime: this.state.datetime
+    });
+  }
+
+  updateTheirValue(evt){
+    this.state={their: evt.target.value};   
+  }
+
+  updateUserNameValue(evt){
+    this.state={userName: evt.target.value};   
+  }
+
   loadSearch(){
     return (
         <div className="Searchpage">
           <div id="logo-div" className="column">
           <a id="back" onClick={this.backToHome}><img src={arrowLeft} alt="go-back" /></a>        
-          
             <img id="logo" src={logo} alt="boosala logo"/>
             <div id="panel" className={this.state.language==="arabic" ? "ArTextDirection" : " "}>
                 <div className="text">
                   {this.state.text[this.state.language][0]} 
-                  <input id="me" name="me" placeholder={this.state.text[this.state.language][1]}  required/>
+                  <input id="me" placeholder={this.state.text[this.state.language][1]} 
+                      onChange={this.updateUserNameValue} required/>
                   {this.state.text[this.state.language][2]}
                 </div>
                 <div className="text">
                   {this.state.text[this.state.language][3]}
-                  <input id="their" name="him" placeholder={this.state.text[this.state.language][4]} required />.
+                  <input id="their" placeholder={this.state.text[this.state.language][4]} 
+                      onChange={this.updateTheirValue} required/>.
                 </div>
             </div>
             <div id="upload">
                 <label for="fileToUpload">
                     <img id="face" src={this.state.face[this.state.language]} alt="boosala logo"/>
                 </label>
-                <input id="fileToUpload" type="file" name="fileToUpload" onchange="readURL(this);" />
-                <button id="submit" name="submit">{this.state.findButton[this.state.language]}</button>
+                <input id="fileToUpload" type="file" name="fileToUpload" />
+                <button id="submit" name="submit" onClick={this.loadResult}>{this.state.findButton[this.state.language]}</button>
             </div>
           </div>
         </div>
@@ -70,7 +96,12 @@ class Search extends Component {
     return (
       <div className="Search">
         {this.state.current === "search" ? this.loadSearch() : null}
-        {this.state.current === "home" ? <App/> : null}
+        {this.state.current === "home" ? <App language={this.state.language}/> : null}
+        {this.state.current === "result" ? <Result language={this.state.language}
+                                            their={this.state.their}
+                                            userName={this.state.userName}
+                                            location={this.state.location}
+                                            datetime={this.state.datetime} /> : null}
       </div>
     );
   }
